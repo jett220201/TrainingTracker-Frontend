5f9d6de69d3cc533aeebcdf89512830c8b4b4b78
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-08-21 19:24:13 -0500

    Create axios client

---
+++ src/api/axiosClient.ts
@@ -0,0 +1,42 @@
+import axios, { AxiosError, type AxiosResponse } from "axios";
+import type { ErrorResponse } from "../types/dto/ErrorResponse";
+
+const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  withCredentials: true,
+});
+
+axiosClient.interceptors.response.use(
+  (response : AxiosResponse) => response,
+  async (error: AxiosError<ErrorResponse>) => {
+    const originalRequest = error.config as any;
+
+    if (error.response?.status === 401 && !originalRequest._retry) { // Handle unauthorized access
+      originalRequest._retry = true;
+      try {
+        await axiosClient.post("/auth/refresh");
+        return axiosClient(originalRequest); // Retry the request
+      }
+      catch (refreshError) {
+        console.error("Refresh token failed, redirecting to login", refreshError);
+        window.location.href = "/login";
+      }
+    }
+
+    switch (error.response?.status) {
+      case 400:
+        console.error("Bad Request:", error.response.data);
+        break;
+      case 500:
+        console.error("Internal Server Error:", error.response.data);
+        break;
+      default:
+        console.error("An error occurred:", error.response?.data);
+        break;
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosClient;
