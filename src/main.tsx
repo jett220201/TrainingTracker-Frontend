01c5adda1c5cdc8eb1dd445fa9fe3e1c1d2e7b08
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-08-21 19:23:58 -0500

    Create router with layouts and pages in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,49 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PublicLayout from './layouts/PublicLayout.tsx'
+import PrivateLayout from './layouts/PrivateLayout.tsx'
+import Login from './pages/Login.tsx';
+import Register from './pages/Register.tsx';
+import ForgotPassword from './pages/ForgotPassword.tsx';
+import Home from './pages/home.tsx';
+import Workouts from './pages/Workouts.tsx';
+import NewWorkout from './pages/NewWorkout.tsx';
+import Goals from './pages/Goals.tsx';
+import NewGoal from './pages/NewGoal.tsx';
+import Progress from './pages/Progress.tsx';
+import Exercises from './pages/Exercises.tsx';
+import Profile from './pages/Profile.tsx';
+import Settings from './pages/Settings.tsx';
+
+const router = createBrowserRouter([
+  {
+    element: <PublicLayout />,
+    children: [
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
+      { path: "/forgot-password", element: <ForgotPassword /> },
+    ]
+  },
+  {
+    element: <PrivateLayout />,
+    children: [
+      { path: "/app/home", element: <Home /> },
+      { path: "/app/workouts", element: <Workouts /> },
+      { path: "/app/workouts/new", element: <NewWorkout /> },
+      { path: "/app/goals", element: <Goals /> },
+      { path: "/app/goals/new", element: <NewGoal /> },
+      { path: "/app/progress", element: <Progress /> },
+      { path: "/app/exercises", element: <Exercises /> },
+      { path: "/app/profile", element: <Profile /> },
+      { path: "/app/settings", element: <Settings /> },
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
