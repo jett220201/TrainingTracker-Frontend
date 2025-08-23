305a0562517d65e06d50a251a0a0fdc0d72f78ff
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-08-22 20:44:03 -0500

    Fix alert block component

--- src/components/ui/AlertBlock.tsx
+++ src/components/ui/AlertBlock.tsx
@@ -9,27 +9,37 @@ interface AlertBlockProps {
 }
 
 function getTypeClassname(type: Alert) {
-    let typeColor = "";
     switch (type) {
         case "Error":
-            typeColor = "red";
-            break;
+            return {
+                containerClass: 'flex flex-row border-1 border-red-500 bg-red-50 rounded-md p-2 items-center gap-2 w-90 lg:w-100',
+                iconClass: 'text-red-500 w-5 h-5',
+                titleClass: 'text-red-800',
+                bodyClass: 'text-red-500'
+            };
         case "Success":
-            typeColor = "green";
-            break;
+            return {
+                containerClass: 'flex flex-row border-1 border-green-500 bg-green-50 rounded-md p-2 items-center gap-2 w-90 lg:w-100',
+                iconClass: 'text-green-500 w-5 h-5',
+                titleClass: 'text-green-800',
+                bodyClass: 'text-green-500'
+            };
         case "Warning":
-            typeColor = "yellow";
-            break;
+
+            return {
+                containerClass: 'flex flex-row border-1 border-yellow-500 bg-yellow-50 rounded-md p-2 items-center gap-2 w-90 lg:w-100',
+                iconClass: 'text-yellow-500 w-5 h-5',
+                titleClass: 'text-yellow-800',
+                bodyClass: 'text-yellow-500'
+            };
         default:
-            typeColor = "blue";
-            break;
+            return {
+                containerClass: 'flex flex-row border-1 border-blue-500 bg-blue-50 rounded-md p-2 items-center gap-2 w-90 lg:w-100',
+                iconClass: 'text-blue-500 w-5 h-5',
+                titleClass: 'text-blue-800',
+                bodyClass: 'text-blue-500 whitespace-pre'
+            };
     }
-    return {
-        containerClass: `flex flex-row border-1 border-${typeColor}-500 bg-${typeColor}-50 rounded-md p-2 items-center gap-2 w-90 lg:w-100`,
-        iconClass: `text-${typeColor}-500 w-5 h-5`,
-        titleClass: `text-${typeColor}-800`,
-        bodyClass: `text-${typeColor}-500`
-    };
 }
 
 function AlertBlock({icon: Icon, title, body, type} : AlertBlockProps) {
