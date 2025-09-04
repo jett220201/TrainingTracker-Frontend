3b732629ea50ff0ca0d56f79d2fe753282a3357c
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-09-03 21:13:09 -0500

    Add workout minimal card component

---
+++ src/components/ui/WorkoutMinimalCard.tsx
@@ -0,0 +1,27 @@
+import type { LucideIcon } from "lucide-react";
+
+interface WorkoutMinimalCardProps {
+    icon: LucideIcon;
+    iconBgColor?: string;
+    iconColor?: string;
+    title: string;
+    description: string;
+}
+
+function WorkoutMinimalCard({title, description, icon: Icon, iconBgColor, iconColor}: WorkoutMinimalCardProps) {
+    return (
+        <article className={`bg-white text-white p-4 rounded-lg border border-gray-200 flex w-full gap-8 items-center`}>
+            <section>
+                <div className={`w-14 h-14 flex items-center justify-center bg-${iconBgColor ?? 'gray-100'} p-3 rounded-full`}>
+                    <Icon className={`text-${iconColor ?? 'gray-500'} w-6 h-6`}/>
+                </div>
+            </section>
+            <section>
+                <p className={`text-black font-semibold text-lg`}>{title}</p>
+                <p className={`text-gray-600`}>{description}</p>
+            </section>
+        </article>
+    );
+}
+
+export default WorkoutMinimalCard;
