e60ecefca03d033d8bb7e19fd3cfddd0fcc533df
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-09-10 19:58:50 -0500

    Add dark styles for sidebar

--- src/components/Public/MobileSidebar.tsx
+++ src/components/Public/MobileSidebar.tsx
@@ -3,41 +3,41 @@ import { Link } from "react-router-dom";
 
 function MobileSidebar() {
     return (
-        <div className="block lg:hidden transition-all duration-300 bg-white min-w-screen h-16 p-2 border-t border-gray-200 absolute bottom-0 w-full z-2">
+        <div className="block lg:hidden transition-all duration-300 bg-white dark:bg-slate-950 min-w-screen h-16 p-2 border-t border-gray-200 absolute bottom-0 w-full z-2">
             <ul className="flex flex-row overflow-x-auto scrollbar-hide h-full gap-2">
                 <li className="hover:cursor-pointer w-1/4 flex justify-center items-center flex-shrink-0">
                     <Link to="/app/home">
-                        <LucideHouse className="w-6 h-6 text-blue-700" />
+                        <LucideHouse className="w-6 h-6 text-blue-700 dark:text-gray-200" />
                     </Link>
                 </li>
                 <li className="hover:cursor-pointer w-1/4 flex justify-center items-center flex-shrink-0">
                     <Link to="/app/workouts">
-                        <LucideDumbbell className="w-6 h-6 text-blue-700" />
+                        <LucideDumbbell className="w-6 h-6 text-blue-700 dark:text-gray-200" />
                     </Link>
                 </li>
                 <li className="hover:cursor-pointer w-1/4 flex justify-center items-center flex-shrink-0">
                     <Link to="/app/progress">
-                        <LucideChartLine className="w-6 h-6 text-blue-700" />
+                        <LucideChartLine className="w-6 h-6 text-blue-700 dark:text-gray-200" />
                     </Link>
                 </li>
                 <li className="hover:cursor-pointer w-1/4 flex justify-center items-center flex-shrink-0">
                     <Link to="/app/goals">
-                        <LucideGoal className="w-6 h-6 text-blue-700" />
+                        <LucideGoal className="w-6 h-6 text-blue-700 dark:text-gray-200" />
                     </Link>
                 </li>
                 <li className="hover:cursor-pointer w-1/4 flex justify-center items-center flex-shrink-0">
                     <Link to="/app/exercises">
-                        <LucideBookOpenText className="w-6 h-6 text-blue-700" />
+                        <LucideBookOpenText className="w-6 h-6 text-blue-700 dark:text-gray-200" />
                     </Link>
                 </li>
                 <li className="hover:cursor-pointer w-1/4 flex justify-center items-center flex-shrink-0">
                     <Link to="/app/profile">
-                        <LucideUser className="w-6 h-6 text-blue-700" />
+                        <LucideUser className="w-6 h-6 text-blue-700 dark:text-gray-200" />
                     </Link>
                 </li>
                 <li className="hover:cursor-pointer w-1/4 flex justify-center items-center flex-shrink-0">
                     <Link to="/app/settings">
-                        <LucideSettings className="w-6 h-6 text-blue-700" />
+                        <LucideSettings className="w-6 h-6 text-blue-700 dark:text-gray-200" />
                     </Link>
                 </li>
             </ul>
--- src/components/Public/Sidebar.tsx
+++ src/components/Public/Sidebar.tsx
@@ -1,10 +1,11 @@
-import { LucideBookOpenText, LucideChartLine, LucideDumbbell, LucideGoal, LucideHouse, LucideSettings, LucideUser } from "lucide-react";
+import { LucideBookOpenText, LucideChartLine, LucideDumbbell, LucideGoal, LucideHouse, LucideLogOut, LucideSettings, LucideUser } from "lucide-react";
 import IconLink from "../ui/IconLink";
 import { useTranslation } from "react-i18next";
 import { useAuthStore } from "../../store/AuthStore";
 import { useEffect, useRef, useState } from "react";
 import { Tooltip } from "react-tooltip";
 import 'react-tooltip/dist/react-tooltip.css'
+import { Link } from "react-router-dom";
 
 function Sidebar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -36,75 +37,79 @@ function Sidebar() {
     return (
         <div className="hidden lg:block" ref={sidebarRef}>
             {isOpen ?
-                (<div className="transition-all duration-300 bg-white w-64 min-h-screen p-2 border-r border-gray-200 relative">
+                (<div className="transition-all duration-300 bg-white dark:bg-slate-950 w-64 min-h-screen p-2 border-r border-gray-200 relative">
                     <div className="flex flex-row items-center gap-4 mb-2 p-2 w-80">
                         <img src="/src/assets/logo.png" alt="FitTracker Logo" className="w-12 h-12 " />
-                        <p className="text-xl font-bold text-black">FitTracker</p>
+                        <p className="text-xl font-bold text-black dark:text-white">FitTracker</p>
                     </div>
-                    <hr className="mb-4 bg-gray-500" />
+                    <hr className="mb-4 bg-gray-500 dark:bg-black" />
                     <ul className="flex flex-col gap-1 p-2">
                         <li className="mb-2 hover:cursor-pointer">
-                            <IconLink to="/app/home" icon={LucideHouse} label={t("dashboard")} classname="!text-gray-700 overflow-hidden whitespace-nowrap group-hover:!text-white" />
+                            <IconLink to="/app/home" icon={LucideHouse} label={t("dashboard")} classname="!text-gray-700 dark:!text-gray-200 overflow-hidden whitespace-nowrap group-hover:!text-white" />
                         </li>
                         <li className="mb-2 hover:cursor-pointer">
-                            <IconLink to="/app/workouts" icon={LucideDumbbell} label={t("workouts")} classname="!text-gray-700 overflow-hidden whitespace-nowrap group-hover:!text-white" />
+                            <IconLink to="/app/workouts" icon={LucideDumbbell} label={t("workouts")} classname="!text-gray-700 dark:!text-gray-200 overflow-hidden whitespace-nowrap group-hover:!text-white" />
                         </li>
                         <li className="mb-2 hover:cursor-pointer">
-                            <IconLink to="/app/progress" icon={LucideChartLine} label={t("progress")} classname="!text-gray-700 overflow-hidden whitespace-nowrap group-hover:!text-white" />
+                            <IconLink to="/app/progress" icon={LucideChartLine} label={t("progress")} classname="!text-gray-700 dark:!text-gray-200 overflow-hidden whitespace-nowrap group-hover:!text-white" />
                         </li>
                         <li className="mb-2 hover:cursor-pointer">
-                            <IconLink to="/app/goals" icon={LucideGoal} label={t("achievements")} classname="!text-gray-700 overflow-hidden whitespace-nowrap group-hover:!text-white" />
+                            <IconLink to="/app/goals" icon={LucideGoal} label={t("achievements")} classname="!text-gray-700 dark:!text-gray-200 overflow-hidden whitespace-nowrap group-hover:!text-white" />
                         </li>
                         <li className="mb-2 hover:cursor-pointer">
-                            <IconLink to="/app/exercises" icon={LucideBookOpenText} label={t("exercises")} classname="!text-gray-700 overflow-hidden whitespace-nowrap group-hover:!text-white" />
+                            <IconLink to="/app/exercises" icon={LucideBookOpenText} label={t("exercises")} classname="!text-gray-700 dark:!text-gray-200 overflow-hidden whitespace-nowrap group-hover:!text-white" />
                         </li>
                         <li className="mb-2 hover:cursor-pointer">
-                            <IconLink to="/app/profile" icon={LucideUser} label={t("profile")} classname="!text-gray-700 overflow-hidden whitespace-nowrap group-hover:!text-white" />
+                            <IconLink to="/app/profile" icon={LucideUser} label={t("profile")} classname="!text-gray-700 dark:!text-gray-200 overflow-hidden whitespace-nowrap group-hover:!text-white" />
                         </li>
                         <li className="mb-2 hover:cursor-pointer">
-                            <IconLink to="/app/settings" icon={LucideSettings} label={t("settings")} classname="!text-gray-700 overflow-hidden whitespace-nowrap group-hover:!text-white" />
+                            <IconLink to="/app/settings" icon={LucideSettings} label={t("settings")} classname="!text-gray-700 dark:!text-gray-200 overflow-hidden whitespace-nowrap group-hover:!text-white" />
                         </li>
                     </ul>
                     <footer className="absolute bottom-0 w-full">
-                        <hr className="mb-2 bg-gray-500" />
+                        <Link to="login" className="flex items-center justify-between p-2 w-60 rounded-t group hover:bg-gray-100 dark:hover:bg-gray-700" onClick={() => useAuthStore.getState().logout()}>
+                            <p className="text-gray-700 dark:text-gray-200 dark:group-hover:text-gray-200">{t("logout")}</p>
+                            <LucideLogOut className="w-6 h-6 text-gray-800 dark:text-gray-200 dark:group-hover:text-gray-200 m-2 hover:cursor-pointer" />
+                        </Link>
+                        <hr className="mb-2 bg-gray-500 w-60" />
                         <div className="flex flex-row items-center gap-4 p-2 w-50">
-                            <img src={imgSrc} alt="FitTracker Logo" className="w-12 h-12 mb-2" />
-                            <p className="text-md font-semibold mb-2 text-black whitespace-pre-line">{user?.fullName}</p>
+                            <img src={imgSrc} alt="FitTracker Logo" className="w-12 h-12 mb-2 rounded-lg" />
+                            <p className="text-md font-semibold mb-2 text-black dark:text-white whitespace-pre-line">{user?.fullName}</p>
                         </div>
                     </footer>
                 </div>)
-                : (<div onClick={toggleSidebar} className="transition-all duration-300 bg-white w-16 min-h-screen p-2 border-r border-gray-200 relative">
+                : (<div onClick={toggleSidebar} className="transition-all duration-300 bg-white dark:bg-slate-950 w-16 min-h-screen p-2 border-r border-gray-200 relative">
                     <div className="flex flex-row items-center w-80">
                         <img src="/src/assets/logo.png" alt="FitTracker Logo" className="w-12 h-12 mb-4 mt-2" />
                     </div>
                     <hr className="mb-4 bg-gray-500" />
                     <ul className="flex flex-col gap-6 p-2 pt-4">
                         <li className="mb-2 hover:cursor-pointer" data-tooltip-id="tooltipDashboard" data-tooltip-content={t("dashboard")}>
-                            <LucideHouse className="w-6 h-6 text-gray-800"/>
+                            <LucideHouse className="w-6 h-6 text-gray-800 dark:text-gray-200"/>
                         </li>
                         <Tooltip id="tooltipDashboard" place="right" style={{ backgroundColor: "#1447e6", color: "white", fontWeight: 500 }} />
                         <li className="mb-2 hover:cursor-pointer" data-tooltip-id="tooltipWorkouts" data-tooltip-content={t("workouts")}>
-                            <LucideDumbbell className="w-6 h-6 text-gray-800"/>
+                            <LucideDumbbell className="w-6 h-6 text-gray-800 dark:text-gray-200"/>
                         </li>
                         <Tooltip id="tooltipWorkouts" place="right" style={{ backgroundColor: "#1447e6", color: "white", fontWeight: 500 }} />
                         <li className="mb-2 hover:cursor-pointer" data-tooltip-id="tooltipProgress" data-tooltip-content={t("progress")}>
-                            <LucideChartLine className="w-6 h-6 text-gray-800"/>
+                            <LucideChartLine className="w-6 h-6 text-gray-800 dark:text-gray-200"/>
                         </li>
                         <Tooltip id="tooltipProgress" place="right" style={{ backgroundColor: "#1447e6", color: "white", fontWeight: 500 }} />
                         <li className="mb-2 hover:cursor-pointer" data-tooltip-id="tooltipAchievements" data-tooltip-content={t("achievements")}>
-                            <LucideGoal className="w-6 h-6 text-gray-800"/>
+                            <LucideGoal className="w-6 h-6 text-gray-800 dark:text-gray-200"/>
                         </li>
                         <Tooltip id="tooltipAchievements" place="right" style={{ backgroundColor: "#1447e6", color: "white", fontWeight: 500 }} />
                         <li className="mb-2 hover:cursor-pointer" data-tooltip-id="tooltipExercises" data-tooltip-content={t("exercises")}>
-                            <LucideBookOpenText className="w-6 h-6 text-gray-800"/>
+                            <LucideBookOpenText className="w-6 h-6 text-gray-800 dark:text-gray-200"/>
                         </li>
                         <Tooltip id="tooltipExercises" place="right" style={{ backgroundColor: "#1447e6", color: "white", fontWeight: 500 }} />
                         <li className="mb-2 hover:cursor-pointer" data-tooltip-id="tooltipProfile" data-tooltip-content={t("profile")}>
-                            <LucideUser className="w-6 h-6 text-gray-800"/>
+                            <LucideUser className="w-6 h-6 text-gray-800 dark:text-gray-200"/>
                         </li>
                         <Tooltip id="tooltipProfile" place="right" style={{ backgroundColor: "#1447e6", color: "white", fontWeight: 500 }} />
                         <li className="mb-2 hover:cursor-pointer" data-tooltip-id="tooltipSettings" data-tooltip-content={t("settings")}>
-                            <LucideSettings className="w-6 h-6 text-gray-800"/>
+                            <LucideSettings className="w-6 h-6 text-gray-800 dark:text-gray-200"/>
                         </li>
                         <Tooltip id="tooltipSettings" place="right" style={{ backgroundColor: "#1447e6", color: "white", fontWeight: 500 }} />
                     </ul>
