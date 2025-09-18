1639a63e63a8a6e16acd491481f6b4e1a028a6d5
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-09-17 21:14:42 -0500

    Change color for loading

--- src/components/Public/Loading.tsx
+++ src/components/Public/Loading.tsx
@@ -2,7 +2,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 function Loading() {
     return (
-        <div className="flex justify-center items-center h-full w-full">
+        <div className="flex justify-center items-center h-full w-full bg-white dark:bg-slate-950">
             <CircularProgress size="3rem" />
         </div>
     );
