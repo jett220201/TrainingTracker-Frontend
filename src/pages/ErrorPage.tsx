755d02a7b512ff1a259b2b40d90c9d1227b3027a
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-09-19 18:49:25 -0500

    Add content to 404 and error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,5 +1,18 @@
+import { LucideBug } from "lucide-react";
+import { useTranslation } from "react-i18next";
+
 function ErrorPage() {
-    return <div>Ups something went wrong</div>;
+    const {t} = useTranslation();
+    return (
+        <>
+            <div className="bg-neutral-50 dark:bg-slate-950 h-full flex flex-col lg:flex-row justify-center gap-10 items-center">
+                <section className="flex flex-col items-center text-center gap-5 w-full lg:w-1/2">
+                    <LucideBug className="w-20 h-20 text-red-700"/>
+                    <p className="text-gray-800 dark:text-white lg:line-clamp-2 text-2xl lg:text-5xl text-shadow-lg">{t("errorMessage")}</p>
+                </section>
+            </div>
+        </>
+    );
 }
 
 export default ErrorPage;
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,21 @@
+import { LucideBotOff } from "lucide-react";
+import { useTranslation } from "react-i18next";
+
 function NotFoundPage() {
+    const {t} = useTranslation();
     return (
-        <div>404 hehe</div>
+        <>
+            <div className="bg-neutral-50 dark:bg-slate-950 h-full flex flex-col lg:flex-row justify-center gap-10 items-center">
+                <section className="flex flex-col lg:flex-row justify-center items-center gap-5 lg:w-1/2">
+                    <LucideBotOff className="w-20 h-20 text-blue-700"/>
+                    <p className="text-blue-700 text-9xl text-shadow-lg">404</p>
+                </section>
+                <section className="flex flex-col items-center text-center gap-5 w-full lg:w-1/2">
+                    <p className="text-gray-800 dark:text-white text-4xl lg:text-8xl text-shadow-lg">{':('}</p>
+                    <p className="text-gray-800 dark:text-white lg:line-clamp-2 text-2xl lg:text-5xl text-shadow-lg">{t("404Message")}</p>
+                </section>
+            </div>
+        </>
     );
 }
 
