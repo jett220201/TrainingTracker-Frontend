cf879c5536ee27dd3b2acf2d471919668141a67b
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-09-25 14:21:03 -0500

    Add last details for progress page

--- src/pages/Progress.tsx
+++ src/pages/Progress.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import TitleHeader from "../components/Public/TitleHeader";
 import InfoCard from "../components/ui/InfoCard";
-import { LucideChartPie, LucideFlame, LucidePlus, LucideWeight } from "lucide-react";
+import { LucideChartPie, LucideFlame, LucidePlus, LucideShield, LucideWeight } from "lucide-react";
 import { GET_PROGRESS } from "../api/graphql/queries/progress";
 import { useQuery } from "@apollo/client";
 import Loading from "../components/Public/Loading";
@@ -11,12 +11,26 @@ import { Pagination } from 'swiper/modules';
 import { IconButton } from "../components/ui/IconButton";
 import LineChartGraph from "../components/ui/LineChartGraph";
 import { useTheme } from "../hooks/useTheme";
+import { getBFPRangeLegend, getBMIRangeLegend } from "../utils/fitnessHelper";
+import { Dialog, DialogPanel, DialogTitle, Transition, Description } from "@headlessui/react";
+import { Fragment, useState } from "react";
+import AlertBlock from "../components/ui/AlertBlock";
+import type { Alert } from "../types/general/AlertType";
+import IconInput from "../components/ui/IconInput";
+import { userProgressApi } from "../api/rest/userProgressApi";
+import type { UserProgressRequest } from "../types/dto/UserProgressRequest";
 
 function Progress() {
     const {t} = useTranslation(["common", "progress", "home"]);
     let errorMsg = "";
-    const {data, loading, error} = useQuery(GET_PROGRESS);
+    const {data, loading, error, refetch} = useQuery(GET_PROGRESS);
     const { theme } = useTheme();
+    const [openModalNewProgress, setOpenModelNewProgress] = useState(false);
+    const [message, setMessage] = useState<string | null>(null);
+    const [newWeight, setNewWeight] = useState<number>(0);
+    const [alertType, setAlertType] = useState<Alert>("Tip");
+    const legendBMI = getBMIRangeLegend(t);
+    const legendBFP = getBFPRangeLegend(t);
 
     if(loading) return <Loading />;
 
@@ -32,23 +46,54 @@ function Progress() {
         return `${year}-${month}-${day}`;
     };
 
-    const handleNewProgress = () => {
+    const handleAddNewProgress = async (e : React.FormEvent) => {
+        e.preventDefault();
 
+        if(newWeight <= 0) {
+            setAlertType("Error");
+            setMessage(t("newWeightError", { ns: "progress" }));
+            return;
+        }
+
+        try {
+            const payload = {
+                weight: newWeight
+            }
+            const response = await userProgressApi.add(payload as UserProgressRequest);
+            setAlertType("Success");
+            setMessage(response.message);
+            
+            // refresh data for charts
+            await refetch();
+
+            // hide message
+            setTimeout(()=>{
+                setMessage("");
+                setOpenModelNewProgress(false);
+            }, 3000);
+        }
+        catch (error : any) {
+            setAlertType("Error");
+            setMessage(error.details != undefined ? error.details : error.message)
+        }
     };
 
     return (
         <>
             <TitleHeader title={t("title", { ns: "progress" })} />
-            <div className="bg-neutral-50 dark:bg-slate-950 min-h-[calc(100vh-4rem)] h-100 px-2 py-6 lg:px-8 lg:py-8 overflow-y-auto relative flex flex-col gap-4">
+            <div className="bg-neutral-50 dark:bg-slate-950 min-h-[calc(100vh-4rem)] h-100 px-2 pt-2 pb-18 lg:px-8 lg:pt-8 lg:pb-8 overflow-y-auto relative flex flex-col gap-4">
                 <div>
                     <p className="text-2xl font-bold p-4 text-black dark:text-gray-100">{t("overview", { ns: "progress" })}</p>
-                    <section className="bg-white dark:bg-gray-800 p-4 rounded-lg">
+                    <section className="bg-white dark:bg-gray-800 p-4 rounded-lg border border-gray-200">
                         <Swiper
-                            slidesPerView={3}
+                            breakpoints={{
+                                0: { slidesPerView: 1 }, 
+                                768: { slidesPerView: 3 },
+                            }}
                             spaceBetween={20}
                             pagination={{ clickable: true, dynamicBullets: true }}
                             modules={[Pagination]}
-                            className="w-6xl h-40 pb-5"
+                            className="w-xs lg:w-6xl h-35 lg:h-40 lg:pb-5"
                         >
                             <SwiperSlide>
                                 <InfoCard tooltipId="tooltipWeight" tooltipContent={t("weightTooltip", { ns: "home" })}
@@ -154,55 +199,141 @@ function Progress() {
                 
                 <div>
                     <p className="text-2xl font-bold p-4 text-black dark:text-gray-100">{t("weightProgress", { ns: "progress" })}</p>
-                    <section className="bg-white dark:bg-gray-800 p-4 rounded-lg">
-                        <LineChartGraph
-                            theme={theme}
-                            height={300}
-                            width={700}
-                            labelX={t("date", { ns: "home" })}
-                            labelY={`${t("weight", { ns: "home" })} (kg)`}
-                            color={localStorage.getItem("theme") == "light" ? "#1447e6" : "#3b82f6"}
-                            labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
-                            data={data?.userProgressByUser?.progressEntries?.map((entry: { weight: number; }) => entry.weight)}
-                        />
+                    <section className="bg-white dark:bg-gray-800 p-4 rounded-lg border border-gray-200">
+                        <div className="hidden lg:block">
+                            <LineChartGraph
+                                theme={theme}
+                                height={300}
+                                width={700}
+                                labelX={t("date", { ns: "home" })}
+                                labelY={`${t("weight", { ns: "home" })} (kg)`}
+                                color={localStorage.getItem("theme") == "light" ? "#1447e6" : "#3b82f6"}
+                                labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
+                                data={data?.userProgressByUser?.progressEntries?.map((entry: { weight: number; }) => entry.weight)}
+                            />
+                        </div>
+                        <div className="block lg:hidden">
+                            <LineChartGraph
+                                theme={theme}
+                                height={200}
+                                width={300}
+                                labelX={t("date", { ns: "home" })}
+                                labelY={`${t("weight", { ns: "home" })} (kg)`}
+                                color={localStorage.getItem("theme") == "light" ? "#1447e6" : "#3b82f6"}
+                                labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
+                                data={data?.userProgressByUser?.progressEntries?.map((entry: { weight: number; }) => entry.weight)}
+                            />
+                        </div>
                     </section>
                 </div>
                 
                 <div>
                     <p className="text-2xl font-bold p-4 text-black dark:text-gray-100">{t("bmiProgress", { ns: "progress" })}</p>
-                    <section className="bg-white dark:bg-gray-800 p-4 rounded-lg">
-                        <LineChartGraph
-                            theme={theme}
-                            height={300}
-                            width={700}
-                            labelX={t("date", { ns: "home" })}
-                            labelY={`${t("BMI", { ns: "home" })}`}
-                            color={localStorage.getItem("theme") == "light" ? "#f97316" : "#fdba74"}
-                            labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
-                            data={data?.userProgressByUser?.progressEntries?.map((entry: { bodyMassIndex: number; }) => entry.bodyMassIndex)}
-                        />
+                    <section className="flex flex-col-reverse lg:flex-row items-center justify-center gap-2 lg:gap-10 bg-white dark:bg-gray-800 p-4 rounded-lg border border-gray-200">
+                        {legendBMI.length > 0 && (
+                            <ul className="rounded-lg border border-gray-200 p-2">
+                                {
+                                    legendBMI.map((item, index) => (
+                                        <li key={index} className={`${item.color}`}>{`${item.value} → ${item.description}`}</li>
+                                    ))
+                                }
+                            </ul>
+                        )}
+                        <div className="hidden lg:block">
+                            <LineChartGraph
+                                theme={theme}
+                                height={300}
+                                width={700}
+                                labelX={t("date", { ns: "home" })}
+                                labelY={`${t("BMI", { ns: "home" })}`}
+                                color={localStorage.getItem("theme") == "light" ? "#f97316" : "#fdba74"}
+                                labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
+                                data={data?.userProgressByUser?.progressEntries?.map((entry: { bodyMassIndex: number; }) => entry.bodyMassIndex)}
+                            />
+                        </div>
+                        <div className="block lg:hidden">
+                            <LineChartGraph
+                                theme={theme}
+                                height={200}
+                                width={300}
+                                labelX={t("date", { ns: "home" })}
+                                labelY={`${t("simpleBMI", { ns: "progress" })}`}
+                                color={localStorage.getItem("theme") == "light" ? "#f97316" : "#fdba74"}
+                                labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
+                                data={data?.userProgressByUser?.progressEntries?.map((entry: { bodyMassIndex: number; }) => entry.bodyMassIndex)}
+                            />
+                        </div>
                     </section>
                 </div>
                 
                 <div>
                     <p className="text-2xl font-bold p-4 text-black dark:text-gray-100">{t("bfpProgress", { ns: "progress" })}</p>
-                    <section className="bg-white dark:bg-gray-800 p-4 rounded-lg">
-                        <LineChartGraph
-                            theme={theme}
-                            height={300}
-                            width={700}
-                            labelX={t("date", { ns: "home" })}
-                            labelY={`${t("BFP", { ns: "home" })} %`}
-                            color={localStorage.getItem("theme") == "light" ? "#10b981" : "#6ee7b7"}
-                            labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
-                            data={data?.userProgressByUser?.progressEntries?.map((entry: { bodyFatPercentage: number; }) => entry.bodyFatPercentage)}
-                        />
+                    <section className="flex flex-col-reverse lg:flex-row items-center justify-center gap-2 lg:gap-10 bg-white dark:bg-gray-800 p-4 rounded-lg border border-gray-200">
+                        {legendBFP.length > 0 && (
+                            <ul className="rounded-lg border border-gray-200 p-2">
+                                {
+                                    legendBFP.map((item, index) => (
+                                        <li key={index} className={`${item.color}`}>{`${item.value} → ${item.description}`}</li>
+                                    ))
+                                }
+                            </ul>
+                        )}
+                        <div className="hidden lg:block">
+                            <LineChartGraph
+                                theme={theme}
+                                height={300}
+                                width={700}
+                                labelX={t("date", { ns: "home" })}
+                                labelY={`${t("BFP", { ns: "home" })} %`}
+                                color={localStorage.getItem("theme") == "light" ? "#10b981" : "#6ee7b7"}
+                                labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
+                                data={data?.userProgressByUser?.progressEntries?.map((entry: { bodyFatPercentage: number; }) => entry.bodyFatPercentage)}
+                            />
+                        </div>
+                        <div className="block lg:hidden">
+                            <LineChartGraph
+                                theme={theme}
+                                height={200}
+                                width={300}
+                                labelX={t("date", { ns: "home" })}
+                                labelY={`${t("simpleBFP", { ns: "progress" })} %`}
+                                color={localStorage.getItem("theme") == "light" ? "#10b981" : "#6ee7b7"}
+                                labels={data?.userProgressByUser?.progressEntries?.map((entry: { createdAt: string; }) => formatDate(new Date(entry.createdAt)))}
+                                data={data?.userProgressByUser?.progressEntries?.map((entry: { bodyFatPercentage: number; }) => entry.bodyFatPercentage)}
+                            />
+                        </div>
                     </section>
                 </div>
                 
-                <IconButton icon={LucidePlus} label={t("newProgress", { ns: "progress" })} onClick={handleNewProgress}
-                    classname={"absolute flex items-center text-gray-100 !p-4 !rounded-full bg-blue-700 top-6 right-10 gap-2"}/>
+                <IconButton icon={LucidePlus} label={t("newProgress", { ns: "progress" })} onClick={() => setOpenModelNewProgress(true)}
+                    classname={"hidden lg:flex absolute items-center text-gray-100 !p-4 !rounded-full bg-blue-700 top-6 right-10 gap-2"} />
+
+                <IconButton icon={LucidePlus} label="" onClick={() => setOpenModelNewProgress(true)}
+                    classname={"block lg:hidden absolute text-gray-100 !p-4 !rounded-full bg-blue-700 top-3 right-10"} />
             </div>
+
+            <Transition show={openModalNewProgress} as={Fragment}>
+                <Dialog as="div" onClose={setOpenModelNewProgress} className="relative z-2">
+                    <div className="fixed inset-0 bg-black/30" />
+                    <div className="fixed inset-0 flex items-center justify-center">
+                        <DialogPanel className="w-80 max-w-md lg:w-full transform overflow-hidden rounded-2xl bg-white dark:bg-gray-800 p-6 text-left align-middle shadow-xl transition-all">
+                            <DialogTitle className="text-xl font-bold text-black dark:text-gray-100">{t("newProgress", { ns: "progress" })}</DialogTitle>
+                            <Description className="text-black dark:text-gray-100">{t("newProgressDescription", { ns: "progress" })}</Description>
+                            <form className="flex flex-col gap-4 mt-2 mb-4" onSubmit={handleAddNewProgress}>
+                                <IconInput inputId="newWeight" onChange={(e) => setNewWeight(parseFloat(e.target.value))}
+                                    icon={LucideWeight} type="number" placeholder={t("weightPlaceholder", { ns: "progress" })} label={t("weightLabel", { ns: "progress" })}
+                                    classname="pl-10 w-full p-2 mb-2 border border-gray-200 rounded text-gray-500 dark:text-gray-200" />
+                                <IconButton label={t("saveProgress", { ns: "progress" })} icon={LucideShield}
+                                    classname="flex items-center justify-center text-white w-full gap-4 font-medium bg-linear-to-r from-sky-600 to-blue-800" />
+                            </form>
+                            {message && <AlertBlock icon={LucideShield}
+                                title={""}
+                                body={message}
+                                type={alertType} />}
+                        </DialogPanel>
+                    </div>
+                </Dialog>
+            </Transition>
         </>
     );
 }
--- src/utils/fitnessHelper.ts
+++ src/utils/fitnessHelper.ts
@@ -17,3 +17,63 @@ export function getRangeBFP(bfp: number | undefined, t: (key: string) => string)
     if (bfp >= 25) return t("obese");
     return t("unknown");
 }
+
+export function getBMIRangeLegend(t: (key: string) => string) : Array<any> {
+    return [
+        {
+            value: "< 18.5",
+            description: t("underweight"),
+            color: "text-yellow-500 dark:text-yellow-200"
+        },
+        {
+            value: "18.5 - 24.9",
+            description: t("normalWeight"),
+            color: "text-green-500 dark:text-green-200"
+        },
+        {
+            value: "25 - 29.9",
+            description: t("overweight"),
+            color: "text-orange-500 dark:text-orange-200"
+        },
+        {
+            value: "30 - 39.9",
+            description: t("obesity"),
+            color: "text-red-500 dark:text-red-200"
+        },
+        {
+            value: ">= 40",
+            description: t("morbilityObesity"),
+            color: "text-purple-500 dark:text-purple-200"
+        },
+    ];
+}
+
+export function getBFPRangeLegend(t: (key: string) => string) : Array<any> {
+    return [
+        {
+            value: "< 6",
+            description: t("essentialFat"),
+            color: "text-yellow-500 dark:text-yellow-200"
+        },
+        {
+            value: "6 - 14",
+            description: t("athletes"),
+            color: "text-green-500 dark:text-green-200"
+        },
+        {
+            value: "14 - 18",
+            description: t("fitness"),
+            color: "text-blue-500 dark:text-blue-200"
+        },
+        {
+            value: "18 - 25",
+            description: t("average"),
+            color: "text-orange-500 dark:text-orange-200"
+        },
+        {
+            value: ">= 25",
+            description: t("obese"),
+            color: "text-red-500 dark:text-red-200"
+        },
+    ];
+}
