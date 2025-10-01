b9aaecb9719db62f71ae172e80ad688c10d14136
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-10-01 18:38:06 -0500

    Add graphql workouts query

---
+++ src/api/graphql/queries/workouts.ts
@@ -0,0 +1,26 @@
+import { gql } from "@apollo/client";
+
+export const GET_WORKOUTS = gql`
+query GetWorkoutsByUser ( $search: String ) {
+  workoutsByUser(search: $search) {
+    totalWorkouts
+    workouts {
+      id
+      name
+      workoutExercises {
+        id
+        repetitions
+        sets
+        weight
+        exercise {
+          description
+          id
+          muscleGroup
+          muscleGroupName
+          name
+        }
+      }
+    }
+  }
+}
+`;
