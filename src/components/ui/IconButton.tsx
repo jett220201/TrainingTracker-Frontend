2d55913d03d0518098e5582cb6cc0f7ad14d598f
Juan Esteban Torres Tamayo <jett220201@gmail.com>
2025-10-09 23:26:49 -0500

    Add new parameters for IconButton component

--- src/components/ui/IconButton.tsx
+++ src/components/ui/IconButton.tsx
@@ -4,17 +4,18 @@ interface IconButtonProps {
     icon: LucideIcon
     label: string
     classname: string
+    iconClassname?: string
     onClick?: () => void
     onClickForm?: (e: React.FormEvent) => void
 }
 
-export function IconButton({icon: Icon, label, classname, onClick, onClickForm} : IconButtonProps) {
+export function IconButton({icon: Icon, label, classname, iconClassname, onClick, onClickForm} : IconButtonProps) {
     return (
         <button 
             onClick={onClick ?? onClickForm}
             className={classname}
         >
-            <Icon className="w-4 h-4"/>
+            <Icon className={iconClassname ?? "w-4 h-4"}/>
             {label}
         </button>
     )
